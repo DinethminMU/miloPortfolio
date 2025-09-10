a23c63c52368087083c0b885c82f2efc6f285bbc
DinethminMU <dinethminmu@gmail.com>
2025-09-10 15:37:58 +0530

    NavBar adjustments

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -11,11 +11,11 @@ function NavBar(){
  
             <h1  className="text-3xl md:text-5xl text-amber-600 font-bold italic" style={{fontFamily: "cursive"}}>MILO</h1>
             
-            <ul className="flex w-[400px] justify-end">
-                <a href= "#home"><li  className="p-4 hover:scale-120 duration-75 hover:bg-white/30 rounded-xl hover:cursor-pointer">Home</li></a>
-                <a href= "#about"><li  className="p-4 hover:scale-120 duration-75 hover:bg-white/30 rounded-xl hover:cursor-pointer">About</li></a>
-                <a href= "#projects"><li  className="p-4 hover:scale-120 duration-75 hover:bg-white/30 rounded-xl hover:cursor-pointer">Projects</li></a>
-                <a href= "#contact"><li  className="p-4 hover:scale-120 duration-75 hover:bg-white/30 rounded-xl hover:cursor-pointer">Contact</li></a>
+            <ul className="flex w-[200px] justify-end gap-2 md:gap-0">
+                <a href= "#home"><li  className="md:p-4 p-1 hover:scale-120 duration-75 hover:bg-white/30 rounded-xl hover:cursor-pointer">Home</li></a>
+                <a href= "#about"><li  className="md:p-4 p-1 hover:scale-120 duration-75 hover:bg-white/30 rounded-xl hover:cursor-pointer">About</li></a>
+                <a href= "#projects"><li  className="md:p-4 p-1 hover:scale-120 duration-75 hover:bg-white/30 rounded-xl hover:cursor-pointer">Projects</li></a>
+                <a href= "#contact"><li  className="md:p-4 p-1 hover:scale-120 duration-75 hover:bg-white/30 rounded-xl hover:cursor-pointer">Contact</li></a>
             </ul>
            
 
